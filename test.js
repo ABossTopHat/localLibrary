fde5a21484044cb3a48111c92a6a13d91ef45b63
Jacob Boss <125599727+ABossTopHat@users.noreply.github.com>
2023-03-22 09:34:53 -0700

    first commit, i hope this works haha

---
+++ test.js
@@ -0,0 +1,13 @@
+const products0 = []
+const products1 = [
+    {
+    name: "Slip Dress",
+    priceInCents: 8800,
+    availableSizes: [ 0, 2, 4, 6, 10, 12, 16 ]
+  }
+]
+  console.log(products0.length)
+
+  if (products0.length === 0){
+    console.log('yeet')
+  }
